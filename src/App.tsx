0750d6739d557b24b22bf8938aaa7e9be986e6b2
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-10-03 06:46:13 -0300

    Refactor App.tsx and .editorconfig files

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import axios from 'axios';
+
+export default function App() {
+
+    const [latitude, setLatitude] = useState(0)
+    const [longitude, setLongitude] = useState(0)
+    const [pokemons, setPokemons] = useState([])
+
+    useEffect(() => {
+        getCurrentLocation()
+    }, [])
+    
+    useEffect(() => {
+        axios.get("http://192.168.0.37:3000/pokemons")
+        .then((response) => {
+            setPokemons(response.data)
+        })
+         .catch(() => Alert.alert("Houve um erro ao pegar os pokemons"))
+    }, [])
+
+    async function getCurrentLocation() {
+        const response = await Location.requestForegroundPermissionsAsync()
+
+        if (response.granted === true) {
+
+            const myLocation = await Location.getCurrentPositionAsync()
+
+            setLatitude(myLocation.coords.latitude)
+            setLongitude(myLocation.coords.longitude)
+
+            } else {
+                Alert.alert("AVISO", "Sem a permissão, não conseguimos pegar a sua localização :(")
+            }
+        }
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <Text>Usando Maps</Text>
+
+            <MapView 
+                style={{ width: 340, height: 600 }}
+                initialRegion={{
+                    latitude: -7.5529504,
+                    longitude: -48.8832875,
+                    latitudeDelta: 1,
+                    longitudeDelta: 1
+                }}
+            >
+                <Marker 
+                    coordinate={{
+                        latitude: -7.5529504,
+                        longitude: -48.8832875
+                    }}
+                    title="Pokemon"
+                    description="Pokemon encontrado"
+                />
+            </MapView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
